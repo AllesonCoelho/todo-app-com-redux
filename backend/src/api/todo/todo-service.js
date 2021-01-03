b5b3fa95f2737619118e37c96ec40701fb287c65
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-03 18:58:27 -0300

    Inicio do projeto

---
+++ backend/src/api/todo/todo-service.js
@@ -0,0 +1,8 @@
+const todo = require('./todo')
+const Todo = require('./todo')
+
+Todo.methods(['get', 'post', 'put', 'delete'])
+Todo.updateOptions({new: true, runValidators: true})
+
+
+module.exports = Todo
---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa Historia</h2>
+        <p>Elit aliqua aute reprehenderit fugiat Lorem minim eiusmod cupidatat sint.</p>
+        <h2>Missao e Visao</h2>
+        <p>Consequat adipisicing voluptate aliquip fugiat proident. Lorem nostrud laboris irure culpa anim consectetur adipisicing est. Ut sit sit laboris magna minim est commodo aute. Cupidatat Lorem excepteur irure incididunt Lorem.</p>
+        <h2>Imprensa</h2>
+        <p>Consequat adipisicing voluptate aliquip fugiat proident. Lorem nostrud laboris irure culpa anim consectetur adipisicing est. Ut sit sit laboris magna minim est commodo aute. Cupidatat Lorem excepteur irure incididunt Lorem.</p>
+    </div>
+)
