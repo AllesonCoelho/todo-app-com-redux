72a23ea2d64448ddac4cb723eacec944c17c8a0c
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-03 21:49:33 -0300

    Configurando o Redux

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,27 @@
+import {combineReducers} from 'redux'
+
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler livro',
+        list: [{
+            _id: 1,
+            description: 'Pagar fatura do cartao',
+            done: true
+        },{
+            _id: 2,
+            description: 'Reuniao com a equipe as 10',
+            done: false
+        },{
+            _id: 3,
+            description: 'Consulta media na terca depois do almoco',
+            done: false
+        }]
+
+
+    })
+
+})
+
+
+export default rootReducer
