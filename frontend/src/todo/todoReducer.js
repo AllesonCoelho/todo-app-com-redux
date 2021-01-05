1d7a6c885c39f4f117f4d11cc30a5b2c6149b5f4
AllesonCoelho <alleson_henrique@hotmail.com>
2021-01-04 22:22:30 -0300

    Arrumando action do search

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,21 +1,4 @@
-
-
-const INITIAL_STATE = {
-    description: 'Ler livro',
-        list: [{
-            _id: 1,
-            description: 'Pagar fatura do cartao',
-            done: true
-        },{
-            _id: 2,
-            description: 'Reuniao com a equipe as 10',
-            done: false
-        },{
-            _id: 3,
-            description: 'Consulta media na terca depois do almoco',
-            done: false
-        }]
-}
+const INITIAL_STATE = {description: '', list: []}
 
 
 export default (state =  INITIAL_STATE, action) =>{
@@ -24,8 +7,8 @@ export default (state =  INITIAL_STATE, action) =>{
             return { ...state, description: action.payload }
         
         case 'TODO_SEARCHED':
-            return {...state, list: action.payload.data}
-            
+            return {...state, list: action.payload}
+
         case 'TODO_CLEAR':
                 return {...state, description: ''}
 
